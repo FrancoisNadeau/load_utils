d61b1f676edb5d08b934bbbb658f53aab9f56043
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-04-16 01:52:39 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,10 +8,10 @@ from .filterlist_exc import filterlist_exc
 from .filterlist_inc import filterlist_inc
 from .find_key import find_key
 from .get_dst_path import get_dst_path
+from .loaddirs import loaddirs
 from .loadfiles import loadfiles
 from .loadimages import loadimages
 from .load_module import load_module
 from .megamerge import megamerge
 from .evenodd import evenodd
 from .flatten import flatten
-
