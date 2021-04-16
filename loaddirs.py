6f3cd049aa774970475dcbf93510ba091930b3bd
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-04-16 01:58:52 -0400

    Update loaddirs.py

--- loaddirs.py
+++ loaddirs.py
@@ -20,14 +20,14 @@ def loaddirs(
     """
     dirlist = []
     for allfiles in os.walk(src_path):
-        for adir in allfiles[2]:
-          output = os.path.join(allfiles[0], adir)
-          if os.path.isdir(output):
-            dirlist.append(output)
+        for adir in allfiles[1]:
+            output = os.path.join(allfiles[0], adir)
+            if os.path.isdir(output):
+                dirlist.append(output)
     return dirlist
 
 def main():
-  loaddirs(src_path)
-  
+    loaddirs(src_path = '')
+
 if __name__ == '__main__':
-  main()
+    main()
