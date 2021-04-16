27cc82c2f9fad0cb1c21a77bf6d895900994823f
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-04-16 01:52:00 -0400

    Create loaddirs.py

---
+++ loaddirs.py
@@ -0,0 +1,33 @@
+#!usr/bin/env/python3
+
+import os
+from typing import Union
+
+def loaddirs(
+  src_path: Union[os.PathLike, str]
+) -> list:
+    """ Recursively lists the full relative path of all
+        directories in a directory.
+        Only lists files, not directories.
+        ----------------------------------
+        Parameters
+        ----------
+        src_path: Path of directory to be scanned
+        -----------------------------------------
+        Returns:
+        --------
+        dirlist: 1D list full relative paths
+    """
+    dirlist = []
+    for allfiles in os.walk(src_path):
+        for adir in allfiles[2]:
+          output = os.path.join(allfiles[0], adir)
+          if os.path.isdir(output):
+            dirlist.append(output)
+    return dirlist
+
+def main():
+  loaddirs(src_path)
+  
+if __name__ == '__main__':
+  main()
