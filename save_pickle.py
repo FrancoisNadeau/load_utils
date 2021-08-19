ff50d71d74120f88cdf9dc2c80659d7100a7c871
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-08-19 13:15:01 -0400

    Update save_pickle.py

--- save_pickle.py
+++ save_pickle.py
@@ -5,10 +5,10 @@ from typing import Union
 import pickle
 
 def save_pickle(fpath:Union[str,os.PathLike]
-               obj) -> None:
+                your_obj:object) -> None:
     import pickle
     with open(fpath, 'wb') as afile:
-        pickle.dump(afile, obj)
+        pickle.dump(obj=your_obj, file=afile)
     afile.close()
 
 def main():
