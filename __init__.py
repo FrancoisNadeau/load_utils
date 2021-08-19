5e8327a42c2127813112fc084dd76f795390583e
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-08-19 13:08:35 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,9 +12,13 @@ from .loaddirs import loaddirs
 from .loadfiles import loadfiles
 from .loadimages import loadimages
 from .load_module import load_module
+from .load_pickle import load_pickle
 from .lst_intersection import lst_intersection
 from .megamerge import megamerge
 from .evenodd import evenodd
 from .flatten import flatten
+from .read_json import read_json
 from .removeEmptyFolders import removeEmptyFolders
+from .save_json import save_json
+from .save_pickle import save_pickle
 from .sizeof_fmt import sizeof_fmt
