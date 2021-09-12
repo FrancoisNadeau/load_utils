7e881cb9d558dc25e985d758320c7430816f15a5
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-09-12 10:07:51 -0400

    Update filterlist_inc.py

--- filterlist_inc.py
+++ filterlist_inc.py
@@ -1,22 +1,19 @@
 #!usr/bin/env/python3
-
 from typing import Sequence
-from typing import Union
-
 def filterlist_inc(
     include: Sequence,
-    str_lst: Union[list, tuple]
+    lst: Sequence
 ) -> list:
     """
     Returns a tuple of elements including only
     items matching patterns in 'include'
     Parameters
     ----------
-    include: patterns susceptible to return a match among items in 'str_lst'
-    str_lst: list of items to be filtered
+    include: patterns susceptible to return a match among items in 'lst'
+    lst: list of items to be filtered
     """
-    return [itm for itm in str_lst if any(sub in itm for sub in include)]
+    return [itm for itm in lst if any(sub in itm for sub in include)]
 
 def main():
     if __name__ == __main__:
-        filterlist_inc(exclude, str_lst)
+        filterlist_inc(exclude, lst)
