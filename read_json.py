a246032e3aee241e4bd2f6427f8dab040253f73f
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2021-10-03 14:04:06 -0400

    Update read_json.py

--- read_json.py
+++ read_json.py
@@ -4,14 +4,15 @@ import os
 from typing import Union
 import json
 
-def read_json(fpath:Union[str, os.PathLike]) -> dict:
-    with open(fpath, mode='r', encoding='UTF-8') as jfile:
+def read_json(fpath:Union[str, os.PathLike],
+              encoding='UTF-8') -> dict:
+    with open(fpath, mode='r', encoding=encoding) as jfile:
         jdict = json.load(jfile)
     jfile.close()
     return jdict
 
 def main():
-    return read_json(fpath)
+    return read_json(fpath, encoding)
  
 if __name__ == "__main__":
     main()
